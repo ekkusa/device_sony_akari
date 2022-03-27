91773276815b6427ac7e779e258a66f816e1eb19
Ubuntu <azureuser@Ubunthu.jfxodrr00m0e1at3k2gfrtfumf.oslx.internal.cloudapp.net>
2022-03-27 02:43:04 +0000

    akari: lineage > aosp

--- aosp_akari.mk
+++ aosp_akari.mk
@@ -7,10 +7,7 @@
 # Inherit from akari device
 $(call inherit-product, device/sony/akari/device.mk)
 
-# Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
-
-PRODUCT_NAME := lineage_akari
+PRODUCT_NAME := aosp_akari
 PRODUCT_DEVICE := akari
 PRODUCT_MANUFACTURER := Sony
 PRODUCT_BRAND := Sony
