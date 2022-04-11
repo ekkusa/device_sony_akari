f3f9f3ee321b68ae8fd6c3b23aba79c5b7ac8bb3
alief irawan <aliefirawan2@gmail.com>
2022-04-11 13:40:47 +0000

    akari: Add prebuilt kernel

--- device.mk
+++ device.mk
@@ -13,6 +13,14 @@ $(call inherit-product-if-exists, vendor/sony/akari/akari-vendor.mk)
 TARGET_SCREEN_HEIGHT := 2160
 TARGET_SCREEN_WIDTH := 1080
 
+# Kernel
+ifeq ($(TARGET_PREBUILT_KERNEL),)
+    LOCAL_KERNEL := $(LOCAL_PATH)/Image.gz-dtb
+else
+    LOCAL_KERNEL := $(TARGET_PREBUILT_KERNEL)
+endif
+
+PRODUCT_COPY_FILES += $(LOCAL_KERNEL):kernel
 PRODUCT_VENDOR_KERNEL_HEADERS := $(LOCAL_PATH)/kernel-headers
 
 # Overlays
